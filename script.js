0df5eade1eecde0ca4722c2d98c98cf7abe42766
FreeMan <zelonaire@gmail.com>
2023-02-09 12:50:28 +0000

    Questions

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 "use strict";
+
+
+//PROBLEM
+//We work for a company n=building a smart home thermometre.
+//Our most recent task is this:
+//"Given an array of temperatures of one day, calculate the temperature amplitude"
+//"Keep in mind that sometimes there might be a sensor error"
